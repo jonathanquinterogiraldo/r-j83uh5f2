4a0d5775ece97f460cf0d7a5e48fbeb85581e3a0
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-06-17 20:25:55 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.OnKeyUpHandler = this.OnKeyUpHandler.bind(this)
+    this.state = { contador: 0 }
+  }
+  
+  OnKeyUpHandler(event){    
+    this.setState({
+      contador: event.target.value.length
+    })
+  }
+  
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3"  onKeyUp={ this.OnKeyUpHandler }></textarea>
+        <div className="counter">{ this.state.contador }</div>
       </div>
     );
   }
